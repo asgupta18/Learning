ebd4a54fb61660ec52c2bfaa8592153a84499d1f
Ashish <ashish.gupta@netgear.com>
2022-02-24 22:22:04 +0530

    initial commit

---
+++ src/InheritanceDemos/Employee.java
@@ -0,0 +1,32 @@
+package InheritanceDemos;
+
+public class Employee {
+    int salary = 20000;
+
+    void displayEmployeeWork()
+    {
+        System.out.println("Employee Work");
+    }
+
+}
+
+class Programmer extends Employee
+{
+    int bonus = 5000;
+
+    void displayProgrammerWork()
+    {
+        System.out.println("Programmer Work");
+    }
+
+    public static void main(String[] args) {
+        Programmer p = new Programmer();
+        System.out.println(p.bonus);
+        p.displayProgrammerWork();
+
+        System.out.println(p.salary);
+        p.displayEmployeeWork();
+
+    }
+
+}
